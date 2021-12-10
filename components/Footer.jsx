19cb87b04dd9d22c9cfa7ee068010b9afe9a37cc
YMP-BLACRUSH <67401418+YMP-BLACRUSH@users.noreply.github.com>
2021-12-10 14:34:22 +0700

    Footer Component

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,10 +1,49 @@
+import styles from '../styles/Footer.module.css';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const Footer = () => {
   return (
-   <>
-   Footer
-   </> 
-  )
+    <div className={styles.container}>
+      <div className={styles.cardL}>
+        <h1 className={styles.title}>AV0CAD0 CREATIVES.</h1>
+        <h1 className={styles.linkTitle}>
+          <a href="/contact" className={styles.link} passHref>
+            <>
+              <span className={styles.linkText}>WORK WITH US</span>
+              <Image
+                src="/img/link.png"
+                width="40px"
+                height="40px"
+                alt=""
+              />
+            </>
+          </a>
+        </h1>
+      </div>
+      <div className={styles.cardS}>
+        <div className={styles.cardItem}>
+          45 ADAM STREET,
+          <br /> USA
+        </div>
+        <div className={styles.cardItem}>
+          CONTACT@AVOCAD0.DEV
+          <br /> 111_438_5244
+        </div>
+      </div>
+      <div className={styles.cardS}>
+        <div className={styles.cardItem}>
+          FOLLOW US:
+          <br /> __FB __IN __BE __TW
+        </div>
+        <div className={styles.cardItem}>
+          © 2022 AVOCAD0 INTERACTIVE,
+          <br />
+          ALL RIGHTS RESERVED
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
