1ae8d69a2762179281bb27f87c8c2c2b9f65869a
YMP-BLACRUSH <67401418+YMP-BLACRUSH@users.noreply.github.com>
2021-12-19 15:22:43 +0700

    Responsive Hamburger Menu

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,27 +1,52 @@
-import style from "../styles/Navbar.module.css";
+import styles from "../styles/Navbar.module.css";
 import Link from "next/link";
+import { useState } from "react";
 
 const Navbar = () => {
 
+  const [open, setOpen] = useState(false);
+
   return (
-    <div className={style.container}>
+    <div className={styles.container}>
       <Link href="/">AV0CAD0</Link>
-      <ul className={style.list}>
-        <li className={style.listItem}>
+      <ul className={styles.list}>
+        <li className={styles.listItem}>
           <Link href="/products/design">DESIGN</Link>
         </li>
-        <li className={style.listItem}>
+        <li className={styles.listItem}>
           <Link href="/products/development">DEVELOPMENT</Link>
         </li>
-        <li className={style.listItem}>
+        <li className={styles.listItem}>
           <Link href="/products/production">PRODUCTION</Link>
         </li>
-        <li className={style.listItem}>
+        <li className={styles.listItem}>
           <Link href="/products/photography">PHOTOGRAPHY</Link>
         </li>
-        <li className={style.listItem}>
+        <li className={styles.listItem}>
           <Link href="/contact">CONTACT</Link>
         </li>
+      </ul>
+      <div className={styles.hamburger} onClick={() => setOpen(!open)} >
+        <div className={styles.line} />
+        <div className={styles.line} />
+        <div className={styles.line} />
+      </div>
+      <ul onClick={() => setOpen(false)} className={styles.menu} style={{ right: open ? "0px" : "-50vw" }}>
+          <li className={styles.menuItem}>
+            <Link href="/products/design">DESIGN</Link>
+          </li>
+          <li className={styles.menuItem}>
+            <Link href="/products/development">DEVELOPMENT</Link>
+          </li>
+          <li className={styles.menuItem}>
+            <Link href="/products/production">PRODUCTION</Link>
+          </li>
+          <li className={styles.menuItem}>
+            <Link href="/products/photography">PHOTOGRAPHY</Link>
+          </li>
+          <li className={styles.menuItem}>
+            <Link href="/contact">CONTACT</Link>
+          </li>
       </ul>
     </div>
   );
